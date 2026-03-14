9b2cd24732410ba1fad84d894afa4193bd206ed6
Master P <manqoba.mashego@imaginescholar.org>
2026-03-14 12:45:48 +0200

    Added the CV section

--- components/cvSection.jsx
+++ components/cvSection.jsx
@@ -5,6 +5,7 @@ import ConsultationForm from './consultationForm';
 import Reveal from './reveal';
 import FormSection from './formSections/formSection';
 import { FileText } from 'lucide-react';
+import UploadFile from './ui/uploadFile';
 
 const CVSection = () => {
   const [hasCV, setHasCV] = useState("");
@@ -35,46 +36,16 @@ const CVSection = () => {
 						</label>
 					</div>
 				</div>
+				<div className='mt-7'>
+					{hasCV === "yes" && <UploadFile />}
+					{hasCV === "no" && <p>Under construction...</p>}
+				</div>
 			</FormSection>
 		</Reveal>
       </div>
     </div>
   )
 
-  // return (
-  //   <div className="py-24 bg-gray-50">
-  //     <div className="max-w-4xl mx-auto px-6">
-
-  //       {hasCV === null && (
-  //         <div className="bg-white p-10 rounded-2xl shadow-lg text-center">
-  //           <h2 className="text-3xl font-bold mb-6">
-  //             Do you already have a CV?
-  //           </h2>
-
-  //           <div className="flex justify-center gap-6">
-  //             <button
-  //               onClick={() => setHasCV(true)}
-  //               className="bg-[#faa329] text-white cursor-pointer px-8 py-3 rounded-lg font-semibold hover:bg-[#e6951f] transition"
-  //             >
-  //               YES
-  //             </button>
-
-  //             <button
-  //               onClick={() => setHasCV(false)}
-  //               className="border border-gray-300 cursor-pointer px-8 py-3 rounded-lg font-semibold hover:bg-gray-100 transition"
-  //             >
-  //               NO
-  //             </button>
-  //           </div>
-  //         </div>
-  //       )}
-
-  //       {hasCV === true && <ConsultationForm />}
-  //       {hasCV === false && <PreCVForm />}
-
-  //     </div>
-  //   </div>
-  // );
 };
 
 export default CVSection;
